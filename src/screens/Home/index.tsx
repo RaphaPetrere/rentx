92cdf9948579234bcfe93a5fec3c9fef471affb6
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-12 12:38:20 -0300

    ✨ Setting Home Header using react-native-responsive-fontsize

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StatusBar } from 'react-native';
 import {
   Container,
-  Title,
+  Header,
+  HeaderContent,
+  TotalCars,
 } from './styles';
 
+import Logo from '../../assets/logo.svg';
+
+import { RFValue } from 'react-native-responsive-fontsize';
+
 export function Home() {
+  const [totalCars, setTotalCars] = useState(0);
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de {totalCars} carros 
+          </TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   )
 }
