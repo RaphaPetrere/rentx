9eda508f21e38d89d9d2dfc72ba5157c841c170f
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-12 16:53:35 -0300

    ✨ Creating Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Title } from './styles'
+
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+interface Props {
+  title: string;
+  color?: string;
+  onPress: () => void;
+}
+
+export function Button({title, color, ...rest}: Props) {
+  return (
+    <GestureHandlerRootView>
+      <Container {...rest} color={color}>
+        <Title>{title}</Title>
+      </Container>
+    </GestureHandlerRootView>
+  )
+}
