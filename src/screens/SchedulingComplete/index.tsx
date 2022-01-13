d3290c5291b4f6eb1304e92fd742f005902e9228
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-12 21:01:34 -0300

    ✨ Creating SchedulingComplete screen

---
+++ src/screens/SchedulingComplete/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+  Container,
+  Content,
+  Title,
+  Message,
+  Footer,
+  ButtonContainer,
+} from './styles'
+import { useWindowDimensions } from 'react-native';
+
+import LogoSvg from '../../assets/logo_background_gray.svg';
+import DoneSvg from '../../assets/done.svg';
+import { Button } from '../../components/Button';
+import { useTheme } from 'styled-components';
+
+export function SchedulingComplete() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <LogoSvg width={useWindowDimensions().width} />
+
+      <Content>
+        <DoneSvg width={80} height={80}/>
+        <Title>Carro alugado!</Title>
+
+        <Message>
+          Agora você só precisa ir {'\n'}
+          até a concessionária da RENTX {'\n'}
+          pegar o seu automóvel.
+        </Message>
+      </Content>
+
+      <Footer>
+        <ButtonContainer>
+          <Button 
+            title='Ok'
+            onPress={() => {}}
+            color={theme.colors.shape_dark}
+          />
+        </ButtonContainer>
+      </Footer>
+    </Container>
+  )
+}
