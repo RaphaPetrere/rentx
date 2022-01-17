56d7900fc3613e95a1a9febfd3420bf9dbdef1d7
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-16 21:25:04 -0300

    ✨ Using KeyboardAvoidingView and TouchableWithoutFeedback to get a better accessibility

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native'
 import { useTheme } from 'styled-components'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
@@ -16,57 +21,66 @@ import {
 export function SignIn() {
   const theme = useTheme();
   return (
-    <Container>
-      <StatusBar 
-        barStyle={'dark-content'}
-        backgroundColor={'transparent'}
-        translucent
-      />
-      <Header>
-        <Title>
-          Estamos{'\n'}
-          quase lá.
-        </Title>
-        <Subtitle>
-          Faça seu login para começar{'\n'}
-          uma experiência incrível.
-        </Subtitle>
-      </Header>
-      
-      <Form>
-        <Input 
-          iconName="mail"
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
-        
-        <Input 
-          iconName="lock"
-          placeholder='Senha'
-          password
-        />
-      </Form>
-
-      <Footer>
-        <Button 
-          title='Login'
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <ButtonWithMargin>
-          <Button 
-            title='Criar conta gratuita'
-            onPress={() => {}}
-            color={theme.colors.background_secondary}
-            enabled={true}
-            loading={false}
-            light
+    <KeyboardAvoidingView
+      behavior='position'
+      enabled
+    >
+      <TouchableWithoutFeedback
+        onPress={Keyboard.dismiss}
+      >
+        <Container>
+          <StatusBar 
+            barStyle={'dark-content'}
+            backgroundColor={'transparent'}
+            translucent
           />
-        </ButtonWithMargin>
-      </Footer>
-    </Container>
+          <Header>
+            <Title>
+              Estamos{'\n'}
+              quase lá.
+            </Title>
+            <Subtitle>
+              Faça seu login para começar{'\n'}
+              uma experiência incrível.
+            </Subtitle>
+          </Header>
+          
+          <Form>
+            <Input 
+              iconName="mail"
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize='none'
+            />
+            
+            <Input 
+              iconName="lock"
+              placeholder='Senha'
+              password
+            />
+          </Form>
+
+          <Footer>
+            <Button 
+              title='Login'
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+            <ButtonWithMargin>
+              <Button 
+                title='Criar conta gratuita'
+                onPress={() => {}}
+                color={theme.colors.background_secondary}
+                enabled={true}
+                loading={false}
+                light
+              />
+            </ButtonWithMargin>
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
