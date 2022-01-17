211a2c1114f2911ba41742326e3dbbfe52db1e73
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-17 11:50:48 -0300

    ✨ Creating Bullet component and using it in place of the image index

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container } from './styles'
+
+interface Props {
+  active?: boolean;
+}
+
+export const Bullet = ({ active = false }: Props) => <Container active={active} />
